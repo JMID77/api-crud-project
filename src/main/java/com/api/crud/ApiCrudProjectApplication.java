5801e5bc8b74a8f8a3c8f8830b5e60999115c155
JMID77 <easi.jmid@gmail.com>
2025-03-28 13:21:25 +0100

    Commit intermediate 2

--- src/main/java/com/api/crud/ApiCrudProjectApplication.java
+++ src/main/java/com/api/crud/ApiCrudProjectApplication.java
@@ -1,4 +1,4 @@
-package com.api.crud.apiCrudProject;
+package com.api.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
