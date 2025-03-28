ac695d058cbff0281c0fe8fd824c310e9b3f081a
JMID77 <easi.jmid@gmail.com>
2025-03-28 11:33:34 +0100

    Initial commit

---
+++ src/main/java/com/api/crud/apiCrudProject/ApiCrudProjectApplication.java
@@ -0,0 +1,13 @@
+package com.api.crud.apiCrudProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrudProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCrudProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/crud/apiCrudProject/ApiCrudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.crud.apiCrudProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
