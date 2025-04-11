36de4c2860ab9a6a09ea4ea009e5962a5b047f71
JMID77 <easi.jmid@gmail.com>
2025-04-11 10:15:42 +0200

    Commit initial

--- mysql-init/init.sql
+++
@@ -1,4 +0,0 @@
--- Création de root@'%' pour permettre l'accès depuis les autres conteneurs
-CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'rootapi';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
